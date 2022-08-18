89504c3ed23564e8b7d22a65fd4e2bd2cec8f5d1
DabbeAsh <106366192+DabbeAsh@users.noreply.github.com>
2022-08-18 11:44:00 +0530

    Add files via upload

---
+++ Cucumber/Activities/StepDef.java
@@ -0,0 +1,25 @@
+package stepDef;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class StepDef{
+    WebDriver driver;
+
+    @Given("^user is on the Amazon website$")
+    public void user_is_on_Home_Page() throws Throwable {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://www.amazon.in");
+    }
+
+    @When("^user searches a product$")
+    public void user_searches_a_product() throws Throwable {
+        driver.findElement(By.xpath(".//*[@id='account']/a")).click();
+    }
+}
